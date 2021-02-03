59d2fc593faedf5ef8e7885c70a133802edb3c7f
allyson <allyson.brandao@venhapranuvem.com.br>
2021-02-03 14:37:27 -0300

    Introdução e setup da API.

---
+++ API/curso/curso.api/Controllers/UsuarioController.cs
@@ -0,0 +1,20 @@
+﻿using curso.api.Models.Usuario;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace curso.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : Controller
+    {
+        [HttpPost]
+        public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+        {
+            return Created(" ", loginViewModelInput);
+        }
+    }
+}
---
+++ API/curso/curso.api/Models/Usuario/LoginViewModelInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace curso.api.Models.Usuario
+{
+    public class LoginViewModelInput
+    {
+        public string Login
+        {
+            get; set;
+        }
+
+        public string Senha
+        {
+            get; set;
+        }
+    }
+}
